0873fd43231b96dd53fe921594fc2250f4096a16
Jayaprabha M <jayaprabha1112006@gmail.com>
2026-08-31 13:58:05 +0530

    Added sample speech activation data

---
+++ speech_features.py
@@ -0,0 +1,17 @@
+speech_features = [
+    -4.2,
+    -3.5,
+    -2.8,
+    -2.1,
+    -1.6,
+    -1.0,
+    -0.5,
+    -0.2,
+    0.0,
+    0.4,
+    1.2,
+    2.5
+]
+
+print("Speech feature activations:")
+print(speech_features)
